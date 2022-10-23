b7fa51679a3e28898c8a3333ea56dcf27908f8c7
laintime <hzhe111@163.com>
2022-10-23 13:08:44 +0800

    leet code 283 move zeros

--- algorithm/leetcode189_RotateArray.py
+++ algorithm/leetcode189_RotateArray.py
@@ -7,8 +7,25 @@ class Solution(object):
         print(nums)
 
 
+def otherSolution(array, n):
+    def reverse(array, l, h):
+        while l <= h:
+            array[l], array[h] = array[h], array[l]
+            l += 1
+            h -= 1
+
+    ln = len(array)
+    n = n % ln
+    # reverse array[:ln-n]
+    reverse(array, 0, ln - n - 1)
+    # reverse array[ln-n:]
+    reverse(array, ln - n, ln - 1)
+    # reverse whole array
+    reverse(array, 0, ln - 1)
+
+
 # test
-nums = [1,2,3,4,5,6,7,8]
-k =3
+nums = [1, 2, 3, 4, 5, 6, 7, 8]
+k = 3
 s = Solution()
 s.rotateArray(nums, k)

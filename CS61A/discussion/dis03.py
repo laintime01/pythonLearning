7b9c2c518698e2c912822c2e0de463aa03d6b6b7
laintime <hzhe111@163.com>
2022-11-04 20:06:34 +0800

    q6

--- CS61A/discussion/dis03.py
+++ CS61A/discussion/dis03.py
@@ -28,3 +28,34 @@ print(is_prime(51))
 
 
 # Q5 Recursive Hailstone
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, and return the number of elements"""
+    print(n)
+    if n == 1:
+        return 1
+    elif n % 2 == 0:
+        return 1 + hailstone(n // 2)
+    else:
+        return 1 + hailstone(n * 3 + 1)
+
+
+print("------Q5--------")
+hailstone(10)
+
+
+# Q6 Merge Numbers
+
+
+def merge(n1, n2):
+    """ Merge two numbers by digit in decreasing order
+    >>> merge(31, 42)
+    4321
+    """
+    if n1 == 0:
+        return n2
+    if n2 == 0:
+        return n1
+    if n1 % 10 < n2 % 10:
+        return merge(n1 // 10, n2) * 10 + n1 % 10
+    else:
+        return merge(n1, n2 // 10) * 10 + n2 % 10

16cc1751c67e910da19a831410a17ff92f189f87
laintime <hzhe111@163.com>
2022-11-18 22:20:47 +0800

    hw05 q2

--- CS61A/homework/HW05/hw05.py
+++ CS61A/homework/HW05/hw05.py
@@ -24,7 +24,19 @@ def merge(a, b):
             yield i
             i = next(a)
 
+
 # Q2 Generate Permutations
 def gen_perms(seq):
     """Generates all permutations of the given sequence.
-    Each permutation is a list of the elements"""
+    Each permutation is a list of the elements
+    >>> sorted(gen_perms([1, 2, 3]))
+    [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+    >>> sorted(gen_perms('ab'))
+    [['a', 'b'], ['b', 'a']]
+    """
+    if not seq:
+        yield []
+    else:
+        for p in gen_perms(seq[1:]):
+            for i in range(len(seq)):
+                yield p[:i] + [seq[0]] + p[i:]

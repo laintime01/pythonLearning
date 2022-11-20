825234724d100735befbd3bf072f1ef6ad7701ab
laintime <hzhe111@163.com>
2022-11-20 20:10:54 +0800

    q3

--- CS61A/discussion/lab07.py
+++ CS61A/discussion/lab07.py
@@ -111,3 +111,45 @@ class Account:
             future += future * self.interest
             ans += 1
         return ans
+
+
+# Q3
+class FreeChecking(Account):
+    """A bank account that charges for withdrawals, but the first two are free!
+    >>> ch = FreeChecking('Jack')
+    >>> ch.balance = 20
+    >>> ch.withdraw(100)  # First one's free. Still counts as a free withdrawal even though it was unsuccessful
+    'Insufficient funds'
+    >>> ch.withdraw(3)    # Second withdrawal is also free
+    17
+    >>> ch.balance
+    17
+    >>> ch.withdraw(3)    # Ok, two free withdrawals is enough
+    13
+    >>> ch.withdraw(3)
+    9
+    >>> ch2 = FreeChecking('John')
+    >>> ch2.balance = 10
+    >>> ch2.withdraw(3) # No fee
+    7
+    >>> ch.withdraw(3)  # ch still charges a fee
+    5
+    >>> ch.withdraw(5)  # Not enough to cover fee + withdraw
+    'Insufficient funds'
+    """
+    withdraw_fee = 1
+    free_withdrawals = 2
+    def __init__(self, account_holder):
+        super().__init__(account_holder)
+        self.withdraws = 0
+
+    def withdraw(self, amount):
+        self.withdraws += 1
+        fee = 0
+        if self.withdraws > self.free_withdrawals:
+            fee = self.withdraw_fee
+        return super().withdraw(amount + fee)
+
+
+
+

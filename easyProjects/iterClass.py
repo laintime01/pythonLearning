85380777d9a1f7804ebb1f27da253c250415f514
laintime <hzhe111@163.com>
2023-02-15 11:06:03 +0800

    add generator iter class

--- easyProjects/iterClass.py
+++ easyProjects/iterClass.py
@@ -35,6 +35,18 @@ class Range7Iterator:
         return num % 7 == 0 or '7' in str(num)
 
 
+# 生成器某种意义上也是迭代器
+def range_7_gen(start, end):
+    num = start
+    while num < end:
+        if num!=0 and (num % 7 == 0 or '7' in str(num)):
+            yield num
+        num += 1
+
+
 r = Range7(0, 40)
 
 print(tuple(r))
+nums = range_7_gen(0, 40)
+print(tuple(nums))
+

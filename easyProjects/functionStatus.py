9317a9ba15d8e4d4406240e93d9e1f08447ac15d
laintime <hzhe111@163.com>
2023-02-16 09:50:55 +0800

    functin status using closure

--- easyProjects/functionStatus.py
+++ easyProjects/functionStatus.py
@@ -1,6 +1,25 @@
 import re
 
 
+# 第一种办法 用闭包函数
+def make_cyclic_mosaic():
+    chars = ['*', 'x']
+    _index = 0
+
+    def _mosaic(strs):
+        nonlocal _index
+        _char = chars[_index]
+        _index = (_index + 1) % len(chars)
+        length = len(strs.group())
+        return _char * length
+
+    return _mosaic
+
+
+t = "商店有100个苹果，小明以12元每斤的价格买走了8个。"
+print(re.sub(r'\d+', make_cyclic_mosaic(), t))
+
+# 第二种办法 用类
 class CyclicMosaic:
     chars = ['*', 'x']
 

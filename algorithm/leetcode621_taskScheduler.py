8fe22906bbe60a2091006b97143999ce5743d85d
laintime <hzhe111@163.com>
2023-02-21 09:49:04 +0800

    invert binary tree

--- algorithm/leetcode621_taskScheduler.py
+++ algorithm/leetcode621_taskScheduler.py
@@ -22,6 +22,7 @@ class Solution(object):
                 if heap:
                     round_task.append(heapq.heappop(heap))
                     round_task[-1][0] += 1
+            print(f'round->{round_task}')
 
             for cnt, task in round_task:
                 if cnt < 0:
@@ -33,6 +34,6 @@ class Solution(object):
                 return res + len(round_task)
 
 
-tasks = ["A", "A", "A", "B", "B", "B"]
+tasks = ["A", "A", "A", "A", "A", "A", "B", "C", "D", "E", "F", "G"]
 n = 2
 print(Solution().taskScheduler(tasks, n))
